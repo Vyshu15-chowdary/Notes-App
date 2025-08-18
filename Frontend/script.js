86c110ad7c6a37b98563dec3bf3f88f76e183b87
vyshnavi <vishuchowdary20@gmail.com>
2025-08-18 19:33:07 +0530

    fetch notes

--- Frontend/script.js
+++ Frontend/script.js
@@ -3,3 +3,28 @@ const noteForm = document.getElementById("note-form");
 const titleInput = document.getElementById("title");
 const contentInput = document.getElementById("content");
 const notesList = document.getElementById("notes-list");
+
+//fetch and display notes
+
+async function fetchNotes(){
+
+    const res = await fetch(API_URL);
+
+    const notes = await res.json();
+
+    notesList.innerHTML="";
+
+    notes.forEach((note)=>{
+        const noteDiv = document.createElement("div");
+        noteDiv.className = "note";
+
+        noteDiv.innerHTML = `<h3>${note.title}</h3>
+        <p>${note.content} </p>
+        <button class="edit-btn" onclick="editNote('${note._id}', '${note.title}', '${note.content}')">Edit</button>
+      <button class="delete-btn" onclick="deleteNote('${note._id}')">Delete</button> `;
+
+      notesList.appendChild(noteDiv);
+
+    });
+
+}
