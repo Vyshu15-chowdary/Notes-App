e1ac9639ad1cc995fcf6397b79b62a70983f27b3
vyshnavi <vishuchowdary20@gmail.com>
2025-08-18 19:23:10 +0530

    getting elements

---
+++ Frontend/script.js
@@ -0,0 +1,5 @@
+
+const noteForm = document.getElementById("note-form");
+const titleInput = document.getElementById("title");
+const contentInput = document.getElementById("content");
+const notesList = document.getElementById("notes-list");
