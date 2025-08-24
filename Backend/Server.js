5fd558ebceed2fbc77df8253604698718b38213b
vyshnavi <vishuchowdary20@gmail.com>
2025-08-24 19:29:57 +0530

    server file setup

---
+++ Backend/Server.js
@@ -0,0 +1,9 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+//import notesmodel
+
+import Notes from "./NotesModel.js";
+
