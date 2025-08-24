341a3fc97f50d89b4bf350de681f2e252459fba4
vyshnavi <vishuchowdary20@gmail.com>
2025-08-24 19:38:49 +0530

    mongodb connection

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,3 +15,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//mongodb connection
+
+mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+})
+
+.then(()=> console.log("Mongodb connected"))
+.catch((err)=> console.log("❌ mongodb connection error:",err));
